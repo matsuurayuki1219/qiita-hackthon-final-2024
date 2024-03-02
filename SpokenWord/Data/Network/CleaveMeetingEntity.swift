6e47f04bc6bf1c8141583d4c626d68416f4810d5
matsuurayuki1219 <yuki.matsuura@progrit.co.jp>
2024-03-02 22:04:48 +0900

    feat: implement data layer.

---
+++ SpokenWord/Data/Network/CleaveMeetingEntity.swift
@@ -0,0 +1,25 @@
+//
+//  CleaveMeetingEntity.swift
+//  SpokenWord
+//
+//  Created by 松浦裕久 on 2024/03/02.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
+
+struct CleaveMeetingEntity: Codable {
+    let id: Int
+    let sentence: String
+    let cleave: Bool
+}
+
+extension CleaveMeetingEntity {
+    func toModel() -> CleaveMeetingModel {
+        return CleaveMeetingModel(
+            id: id,
+            sentence: sentence,
+            cleave: cleave
+        )
+    }
+}
--- SpokenWord/ViewModel.swift
+++ SpokenWord/ViewModel.swift
@@ -6,7 +6,25 @@
 //  Copyright © 2024 Apple. All rights reserved.
 //
 
+import Combine
 
+@MainActor
 class ViewModel {
 
+    @Published var results: [CleaveMeetingModel] = []
+
+    private let repository = CleaveMeetingRepository()
+
+    func postSpeechText(text: String) {
+        Task {
+            do {
+                let response = try await repository.postSpeechMessage(text: text)
+                print(text)
+                results.append(response)
+            } catch {
+                // no-ops
+                // print("サーバとの通信ができていません")
+            }
+        }
+    }
 }
