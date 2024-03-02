6a48619c7db54d471cd5e3b890c924f05e37cb9a
HONG JEONGSEOB <jeongseob.hong@progrit.co.jp>
2024-03-02 16:59:38 +0900

    feat: init

---
+++ QiitaHackthonFinal2024/QiitaHackthonFinal2024App.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaHackthonFinal2024App.swift
+//  QiitaHackthonFinal2024
+//
+//  Created by HONG JEONGSEOB on 2024/03/02.
+//
+
+import SwiftUI
+
+@main
+struct QiitaHackthonFinal2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
